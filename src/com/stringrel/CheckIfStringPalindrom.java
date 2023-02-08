e8444971b1925dfb34b568f475a82b0e05538c76
Azeem Mohammad <mdazeem5790@gmail.com>
2023-02-08 15:57:26 +0530

    Sample Commit to test SSH

--- src/com/stringrel/CheckIfStringPalindrom.java
+++ src/com/stringrel/CheckIfStringPalindrom.java
@@ -3,7 +3,6 @@ package com.stringrel;
 public class CheckIfStringPalindrom {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String s1="ABCDCB";
 		int ctr=0;
 		for(int i=0;i<s1.length()/2;i++) {
