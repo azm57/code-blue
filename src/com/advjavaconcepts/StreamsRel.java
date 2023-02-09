f107cbb051759acc6bcb60f99b3314984cdccce8
Azeem Mohammad <mdazeem5790@gmail.com>
2023-02-09 20:53:44 +0530

    Java Streams

---
+++ src/com/advjavaconcepts/StreamsRel.java
@@ -0,0 +1,16 @@
+package com.advjavaconcepts;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StreamsRel {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> al = new ArrayList<>(Arrays.asList(4, 9, 8, 7, 1, 2, 4, 5));
+		int data = al.stream().filter(n -> n % 2 == 1).map(n -> n * 2).reduce(0, (c, e) -> c + e);
+		System.out.println(data);
+
+	}
+
+}
