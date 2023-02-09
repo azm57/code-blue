12f837fa5d99514576644178bfc4971d0fb0a8ad
Azeem Mohammad <mdazeem5790@gmail.com>
2023-02-09 21:00:23 +0530

    Sample

--- src/com/advjavaconcepts/StreamsRel.java
+++ src/com/advjavaconcepts/StreamsRel.java
@@ -10,6 +10,5 @@ public class StreamsRel {
 		ArrayList<Integer> al = new ArrayList<>(Arrays.asList(4, 9, 8, 7, 1, 2, 4, 5));
 		int data = al.stream().filter(n -> n % 2 == 1).map(n -> n * 2).reduce(0, (c, e) -> c + e);
 		System.out.println(data);
-		
 	}
 }
