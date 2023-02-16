b561300d8645024aa4e1f4c9625cfef0b6c43969
Azeem Mohammad <mdazeem5790@gmail.com>
2023-02-16 17:42:16 +0530

    MergeTwoSortedArrays

---
+++ src/com/intarray/MergeTwoSortedArrays.java
@@ -0,0 +1,39 @@
+package com.intarray;
+
+public class MergeTwoSortedArrays {
+
+	public static void main(String[] args) {
+		int[] intArr1 = { 3, 7, 9, 12, 21 };
+		int[] intArr2 = { 6, 8, 15, 18, 27,30, 35, 38, 39, 48 };
+
+		int arr1Len = intArr1.length;
+		int arr2Len = intArr2.length;
+
+		int[] resArr = new int[arr1Len + arr2Len];
+
+		int arr1Pos, arr2Pos, resArrPos;
+		arr1Pos = arr2Pos = resArrPos = 0;
+
+		while (arr1Pos < arr1Len && arr2Pos < arr2Len) {
+			if (intArr1[arr1Pos] < intArr2[arr2Pos]) {
+				resArr[resArrPos++] = intArr1[arr1Pos++];
+			} else {
+				resArr[resArrPos++] = intArr2[arr2Pos++];
+			}
+		}
+
+		while (arr1Pos < arr1Len) {
+			resArr[resArrPos++] = intArr1[arr1Pos++];
+		}
+
+		while (arr2Pos < arr2Len) {
+			resArr[resArrPos++] = intArr2[arr2Pos++];
+		}
+
+		for (int i : resArr) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
